82523869b5e8bd68c4a6b72b465b238bb73e2c90
Samuel Ocran <sammiestt@gmail.com>
2020-12-28 19:17:13 +0100

    setup worker

---
+++ mailer.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+module.exports.mailer = async (data, from="sammiestt@gmail.com") => {
+  const {friendsEmail, ownersEmail} = data;
+  try {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+    user: process.env.SMTP_AUTH_USER,
+    pass: process.env.SMTP_AUTH_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from,
+    to: friendsEmail,
+    subject: `Access to ${ownersEmail}'s todos`,
+    html: `
+      <div>
+        <p>Hello!</p>
+        <p>You now have full access to ${ownersEmail}'s todos!
+      </div>`
+  };
+
+  const response = await transporter.sendMail(mailOptions);
+
+  return response;
+  } catch (error) {
+
+    return error.message;
+  }
+};
